b037721eaa42bdb8ad5bc187ebd52bd25e017a9f
yann <y.lastapis@gmail.com>
2022-02-02 17:14:48 +0100

    minor

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,26 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { AppVersionControl, ControlButton } from 'ts-lib';
+import { createTheme, ThemeProvider } from '@mui/system';
+
+const theme = createTheme({});
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <ThemeProvider theme={theme} >
+            <div className="App">
+                <header className="App-header">
+                    <img src={logo} className="App-logo" alt="logo"/>
+                    <p>
+                        Edit <code>src/App.tsx</code> and save to reload.
+                    </p>
+                    <AppVersionControl version="2.0.0" env="prod"/>
+                    <ControlButton />
+                </header>
+            </div>
+        </ThemeProvider>
+    );
 }
 
 export default App;
